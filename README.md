1a1d11948964c8079dc4aa911130a52de6761780
root <root@srv571549.hstgr.cloud>
2025-03-10 01:09:10 +0000

    andy

---
+++ README.md
@@ -0,0 +1,1 @@
+# juall? kontol lu gw jual
---
+++ settings.js
@@ -0,0 +1,16 @@
+
+const YOUR_MEMID = "#"; //Memberid Order Kuota 
+const YOUR_KEYORKUT = "#"; //Apikey Order Kuota 
+const YOUR_QR_CODE = "#"; //QR Order Kuota 
+const domain = 'https://ptero.promotewave.tech'; //Your Domain
+const apikey = 'ptla_JxV0st5iL6zLwUOVBJnVlL9AuJEZvqUyAKL6FS7sUJX'; //Your Apikey 
+const KeyApi = ''; //Apikey from https://www.decode.im-rerezz.xyz
+
+module.exports = {
+    YOUR_MEMID,
+    YOUR_KEYORKUT,
+    YOUR_QR_CODE,
+    domain,
+    apikey,
+    KeyApi
+};
