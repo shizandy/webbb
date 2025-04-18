f71355da13ce575326359936169a0aad37cd8860
andybro <132149967+mrlit-a@users.noreply.github.com>
2025-04-18 16:15:57 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@
 const YOUR_MEMID = "#"; //Memberid Order Kuota 
 const YOUR_KEYORKUT = "#"; //Apikey Order Kuota 
 const YOUR_QR_CODE = "#"; //QR Order Kuota 
-const domain = 'https://ptero.promotewave.tech'; //Your Domain
-const apikey = 'ptla_JxV0st5iL6zLwUOVBJnVlL9AuJEZvqUyAKL6FS7sUJX'; //Your Apikey 
+const domain = 'https://marc.mazxa.com'; //Your Domain
+const apikey = 'ptla_yL4Ai4VK9LarWMu71mInkF3YDKkOhiGDC6EasjRXbmm'; //Your Apikey 
 const KeyApi = ''; //Apikey from https://www.decode.im-rerezz.xyz
 
 module.exports = {
