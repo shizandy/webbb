341092c5497c4882e16de4af0973ad488d030014
kenguer <kenguerflore@gmail.com>
2025-06-05 08:25:47 -0400

    settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@
 const YOUR_MEMID = "#"; //Memberid Order Kuota 
 const YOUR_KEYORKUT = "#"; //Apikey Order Kuota 
 const YOUR_QR_CODE = "#"; //QR Order Kuota 
-const domain = 'https://marc.mazxa.com'; //Your Domain
-const apikey = 'ptla_yL4Ai4VK9LarWMu71mInkF3YDKkOhiGDC6EasjRXbmm'; //Your Apikey 
+const domain = 'https://ralph.mazxa.com'; //Your Domain
+const apikey = 'ptla_Kpk7BU3SvJ3ogyUZu77yxOS1g8vfGjP3s2YhxGoSFkK'; //Your Apikey 
 const KeyApi = ''; //Apikey from https://www.decode.im-rerezz.xyz
 
 module.exports = {
